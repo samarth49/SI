e6d77f529ded711c75805aadf01fcf870c93fc2a
Samarth Otari <122641870+samarth49@users.noreply.github.com>
2024-12-23 12:58:08 +0530

    Update ProductDetails.jsx

--- my-app/src/pages/ProductDetails.jsx
+++ my-app/src/pages/ProductDetails.jsx
@@ -17,7 +17,7 @@ const ProductDetails = () => {
       try {
         setLoading(true);
         setError(null);
-        const response = await axios.get(`https://hosteecommerce.vercel.app/product/${id}`);
+        const response = await axios.get(`https://ecommercebackend-liq0.onrender.com/product/${id}`);
         if (response.data.success) {
           setProduct(response.data.product);
         } else {

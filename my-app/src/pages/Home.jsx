d9968737969a53161c03b23aca4f0d9c6a6b427e
Samarth Otari <122641870+samarth49@users.noreply.github.com>
2024-12-23 12:57:48 +0530

    Update Home.jsx

--- my-app/src/pages/Home.jsx
+++ my-app/src/pages/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
 
     const fetchProducts = async (category) => {
         try {
-            const response = await axios.post("https://hosteecommerce.vercel.app/product/category", { category });
+            const response = await axios.post("https://ecommercebackend-liq0.onrender.com/product/category", { category });
             if (response.data.success) {
                 return response.data.products;
             } else {
